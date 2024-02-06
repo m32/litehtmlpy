0223b2e6974c181befc659f952a6f9474e6bb4a2
m32 <grzegorz.makarewicz@gmail.com>
2024-02-06 11:55:03 +0100

    paging for print

---
+++ litehtml-wx-png-paged.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env vpython3
+import logme
+import wx
+from litehtmlpy import litehtmlwx
+
+class LiteHtml(litehtmlwx.LiteHtml):
+    pass
+
+class Main:
+    def __init__(self):
+        self.wxapp = wx.App(False)
+
+    def save(self, i, htmlstart, htmldata, htmlend):
+        cls = LiteHtml()
+        cls.reset()
+        cls.fromString(htmlstart+htmldata+htmlend)
+        cls.render(cls.size[0])
+        cls.size[1] = cls.height()
+        cls.reset()
+
+        cls.draw(
+            0, 0,
+            0, 0, cls.size[0], cls.size[1])
+        cls.bmp.SaveFile(f'demo-{i:04d}.png', wx.BITMAP_TYPE_PNG)
+
+    def main(self):
+        html = open('demo.html', 'rt').read()
+        start = html.find('<body>') + len('<body>')
+        end = html.find('</body>')
+        htmlstart = html[0:start]
+        htmldata = html[start:end]
+        htmlend = html[end:]
+        i = 0
+        split = '<div class="lamstrone" />'
+        while True:
+            istop = htmldata.find(split)
+            if istop == -1:
+                self.save(i, htmlstart, htmldata, htmlend)
+                break
+            self.save(i, htmlstart, htmldata[:istop], htmlend)
+            htmldata = htmldata[istop+len(split):]
+            i += 1
+
+def main():
+    cls = Main()
+    cls.main()
+main()
