2fa485375eee6558195a1049428f8b1526ddab3a
Ondrej Tuma <mcbig@zeropage.cz>
2026-09-17 10:27:01 +0200

    Bind element::parent() and get_attr() to Python
    Needed to walk up from litehtml's hit-tested element (often an inline
    descendant, e.g. a <b> inside an <a>) to find an enclosing link and
    read its href - both were commented out in the bindings, leaving no
    way for a Python container to know what link is under the cursor
    outside of a click (on_anchor_click already gets the resolved url from
    litehtml core directly, but on_mouse_event only gets the element).

--- src/element.h
+++ src/element.h
@@ -10,7 +10,7 @@
 		.def("is_float", &lh::element::is_float)
 		.def("is_block_formatting_context", &lh::element::is_block_formatting_context)
 		.def("is_root", &lh::element::is_root)
-//		.def("parent", &lh::element::parent)
+		.def("parent", static_cast<lh::element::ptr (lh::element::*)() const>(&lh::element::parent))
 //		void						parent(const element::ptr& par)
 		.def("is_table_skip", &lh::element::is_table_skip)
 		.def("children", &lh::element::children)
@@ -32,7 +32,7 @@
 //		virtual void				set_data(const char* data);
 
 //		virtual void				set_attr(const char* name, const char* val);
-//		virtual const char*			get_attr(const char* name, const char* def = nullptr) const;
+		.def("get_attr", &lh::element::get_attr, "name"_a, "def"_a = nullptr)
 //		virtual void				apply_stylesheet(const litehtml::css& stylesheet);
 		.def("refresh_styles", &lh::element::refresh_styles)
 		.def("is_white_space", &lh::element::is_white_space)
